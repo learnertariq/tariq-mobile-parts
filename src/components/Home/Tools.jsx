c1f0d265fcdd9950414d0b403f0e81d2e76b366c
Md Tariqul Islam <programmertariq@gmail.com>
2022-05-22 10:32:30 +0600

    fetched fake data json

--- src/components/Home/Tools.jsx
+++ src/components/Home/Tools.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useQuery } from "react-query";
 import Tool from "./Tool";
+import http from "../../service/http";
 
 const Tools = () => {
+  const { data, isLoading, error } = useQuery("tools", async () => {
+    return await http.get("tools.json");
+  });
   return (
     <section className="container mx-auto mt-6">
       <h2 className="text-center text-4xl">Tools & Parts</h2>
       <div className="tools-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-        <Tool />
-        <Tool />
-        <Tool />
+        {data?.data?.map((tool) => (
+          <Tool />
+        ))}
       </div>
     </section>
   );

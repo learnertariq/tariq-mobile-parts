9a9080efe6dd022de0c71c1f76de6d4fe1909372
Md Tariqul Islam <programmertariq@gmail.com>
2022-05-26 14:27:21 +0600

    add questions

--- src/components/Blogs/QuestionCard.jsx
+++ src/components/Blogs/QuestionCard.jsx
@@ -2,24 +2,18 @@ import React from "react";
 
 const QuestionCard = ({ question }) => {
   return (
-    <div className="question question-1 mt-4 mb-5 px-2 py-5 lg:p-5">
-      <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="order-first lg:order-last">
-          <div className="blog-img-container text-center lg:text-left">
-            <img className="blog-img" src={question.img} alt="" />
-          </div>
+    <div className="question question-1 mt-4 mb-5 px-6 py-5 lg:p-5">
+      <div className="order-first lg:order-last"></div>
+      <div className="qna mx-auto">
+        <div className="q mb-3">
+          <h3 className="text-2xl text-green-700">{question.q}</h3>
         </div>
-        <div className="qna mx-auto">
-          <div className="q mb-3">
-            <h3 className="text-2xl text-green-700">{question.q}</h3>
-          </div>
-          <div className="a">
-            <ul>
-              {question.a.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
+        <div className="a">
+          <ul>
+            {question.a.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
